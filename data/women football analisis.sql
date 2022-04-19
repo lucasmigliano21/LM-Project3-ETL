44e8b7d5aa0dcbe05aad10707a4ac97ce8130341
lucasmigliano21 <lucasmigliano@gmail.com>
2022-04-19 09:47:28 +0200

    updated

--- data/women football analisis.sql
+++ data/women football analisis.sql
@@ -1,7 +1,6 @@
 create database football;
 
-select * from results
-limit 100;
+## import tables 'champions.csv', 'results.csv', 'players,csv' from  "data" folder:
 
 create temporary table wins_year(
 select max(counts) as counts, year
@@ -22,36 +21,29 @@ where year >= 2002
 and winner <> 'None'
 group by year, winner);
 
-create  table winner_year(
+create table winner_year(
 select a.counts, a.year, b.winner from wins_year as a
 join wins_year_country b on a.year=b.year and a.counts=b.counts);
 
-
-select * from winner_year;
-
-select * from players;
-
-select * from champions;
-
--- la posta--
+-- table that has each awarded player with their club, nationality, champion league's champion of that year, and country that won most matches in that year --
 select players.Year, players.Player, players.Club, players.Nationality, champions.Champion, winner_year.winner from players
 join champions on champions.year=players.year
 join winner_year on winner_year.year=players.year
 ;
 
--- jugadoras seleccionadas mejores del mundo y que su seleccion haya ganado la mayor cantidad de partidos en el año --
+-- selected best players in the world and that their team has won the most matches in the year --
 select players.Year, players.Player, players.Club, players.Nationality, champions.Champion, winner_year.winner from players
 join champions on champions.year=players.year
 join winner_year on winner_year.year=players.year
 where players.Club = champions.Champion;
 
--- jugadoras campeonas con su club en champions y seleccionadas mejores del mundo --
+-- players that won the champions league that year and were selected best in the world --
 select players.Year, players.Player, players.Club, players.Nationality, champions.Champion, winner_year.winner from players
 join champions on champions.year=players.year
 join winner_year on winner_year.year=players.year
 where players.Nationality = winner_year.winner;
 
--- jugadoras campeonas con su club en champions, cuya seleccion haya ganado la mayor cantidad de partidos en el año y seleccionadas mejores del mundo -- 
+-- champion players with their club in champions league, whose national team has won the most games in the year and selected best in the world -- 
 select players.Year, players.Player, players.Club, players.Nationality, champions.Champion, winner_year.winner from players
 join champions on champions.year=players.year
 join winner_year on winner_year.year=players.year
